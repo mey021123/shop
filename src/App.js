7faa7eefb6545895e49d175e8e51233a7a67102f
mey021123 <84761169+mey021123@users.noreply.github.com>
2023-08-25 16:08:39 +0900

    230825 문은영 쇼핑몰 파일생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import bg from './img/bg.png';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data);
+  console.log(shoes[0].title);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{ backgroundImage : 'url(' + bg + ')' }}/>
+
+      <div className='container'>
+        <div className='row'>
+        <div className='col-md-4'>
+          <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%'/>
+          {/* <h4>{shoes[0].title}</h4> */}
+          <p>상품설명</p>
+        </div>
+        <div className='col-md-4'>
+          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%'/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </div>
+        <div className='col-md-4'>
+          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%'/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </div>
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+ 
